5eb0751f4995e747d626f8ba840467af1851ae01
Phạm Tấn Phát <phatdroid94@gmail.com>
2018-09-17 21:08:31 +0700

    arr

--- index.php
+++ index.php
@@ -1,6 +1,56 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 //https://github.com/huongnguyen08/php2
 
 echo "Hello World!!";
+echo "<br>";
+$a = 7;
+$b = 5;
+
+echo 'tong hai so $a va $b la $a+$b='.($a+$b);
+
+echo "<br>";
+
+echo gettype($a);
+
+$c = 2.5;
+// $d = "$abc";
+$d = '$abc';
+echo "<br>";
+ 
+echo $d;
+echo "<br>";
+
+$arr = [3,5,7];
+$arr2 = array(2,54.6);
+
+print_r($arr);
+echo "<br>";
+
+class a{
+
+}
+
+$A = new a();
+echo gettype($A);
+echo "<br>";
+
+
+
+$json = json_encode($arr); //array -> string
+$arr3 = json_decode($json);
+
+echo gettype($arr3);
+echo "<br>";
+
+echo $json;
+
+
+
+
+
+
+
 
 ?>
---
+++ index02.php
@@ -0,0 +1,60 @@
+<?php
+
+// $arr = [1.4,'name', 666,45];
+// echo $arr[2];
+// $count = count($arr);
+// for($i=0; $i<$count; $i++){
+//     echo $arr[$i];
+//     echo "<br>";
+// }
+
+// $arr = [
+//     'name1' => 'PHP',
+//     'name2' => 'JS',
+//     'name3' => 'MySQL',
+//     1 => 'Angular'
+// ];
+// echo $arr['name3'];
+// for($i=0; $i<count($arr); $i++){
+//     echo $arr[$i];
+//     echo "<br>";
+// }
+// foreach($arr as $value){
+//     echo $value;
+//     echo "<br>";
+// }
+
+// foreach($arr as $key=>$value){
+//     echo $key . " : " .$value;
+//     echo "<br>";
+// }
+
+
+// echo sizeof($arr);
+// echo "<br>";
+// $a = 'name';
+// print_r($a);
+// echo "<br>";
+?>
+<!-- <?=$a?> -->
+<br>
+<?php //echo $a?>
+
+
+
+<?php
+$arr1 = [1,3,5,7,9];
+
+$arr2 = [
+    'name1' => 'PHP',
+    'name2' => 'JS',
+    'name3' => 'MySQL',
+    1 => $arr1
+];
+$arr3 = [4,6,$arr2];
+
+echo $arr2[1][2];
+echo "<pre>";
+print_r($arr2)
+
+?>
