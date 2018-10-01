f9ee0c20f46c59f8145cf0f86c75436be5c2c903
Phạm Tấn Phát <phatdroid94@gmail.com>
2018-10-01 20:04:46 +0700

    login

--- login/login.php
+++ login/login.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,17 +13,32 @@
         <div class="row justify-content-center">
             <div class="col-6 ">
                 <h2 class="text-center">Login</h2>
+                <?php
+                if(isset($_SESSION['error'])):
+                ?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <?php 
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php
+                endif            
+                ?>
                 <form action="xulylogin.php" method="post">
                     <div class="form-group">
                         <label for="username">Username:</label>
-                        <input type="text" placeholder="Enter username" class="form-control">
+                        <input type="text" name="username" placeholder="Enter username" class="form-control">
                     </div>
                     <div class="form-group">
                         <label for="username">Password:</label>
-                        <input type="password" placeholder="Enter password" class="form-control">
+                        <input type="password" name="password" placeholder="Enter password" class="form-control">
                     </div>
                     <div class="form-group">
-                        <input type="checkbox" id="rememeber">
+                        <input type="checkbox" id="rememeber" name="remember" value="1">
                         <label for="rememeber">Remember me</label>
                     </div>
                     <div class="form-group">
--- login/xulylogin.php
+++ login/xulylogin.php
@@ -1,5 +1,23 @@
-1. login success  => go to home 
-2. fail => back to login
+<?php
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $pass = '111111' ; // pw luc tao tai khoan
+    $hash = md5('khoaphamtraining'.md5($pass)); // luu vao db
 
-admin 11111 
-remember me 
+    if($_POST['username']=='admin' && md5('khoaphamtraining'.md5($_POST['password']))==$hash){
+        $_SESSION['user'] = $_POST['username'];
+        header('Location:home.php');
+    }
+    else{
+        $_SESSION['error'] = "Sai thông tin đăng nhập";
+        header('Location:login.php');
+    }
+}
+else{
+    $_SESSION['error'] = "Vui lòng nhập đầy đủ thông tin";
+    header('Location:login.php');
+}
+
+
+
+?>
