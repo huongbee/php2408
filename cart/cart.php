ca21bcf16fecd28be2e31ab8f7e0c17ad258fedd
Phạm Tấn Phát <phatdroid94@gmail.com>
2018-10-03 21:00:17 +0700

    ajax

--- cart/cart.php
+++ cart/cart.php
@@ -1,3 +1,13 @@
 <?php
+session_start();
 
+$total = 0;
+foreach($_SESSION['cart'] as $sp):
+    $total += $sp['price']*$sp['soluong'];
 ?>
+<h2><?=$sp['name']?></h2>
+<p><?=$sp['price']?> * <?=$sp['soluong']?> = <?=number_format($sp['price']*$sp['soluong'])?></p>
+
+<?php endforeach?>
+
+<h3>Total : <?=$total?></h3>
