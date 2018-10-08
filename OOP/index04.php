292ad4df12d7c2306bf21fbfd5aa52291a84ac5b
Phạm Tấn Phát <phatdroid94@gmail.com>
2018-10-08 20:06:06 +0700

    extends

--- OOP/index04.php
+++ OOP/index04.php
@@ -1,16 +1,19 @@
 <?php
 
 class User{
-    public $name;
+    public $name = "Parent";
     protected $age = 25;
     private $assets = 20000;
 
-    public function __construct($name){
+    function __construct($name){
         $this->name = $name;
     }
     // public function setName($name){
     //     $this->name = $name;
     // }
+    function getName(){
+        return $this->name;
+    }
     function getAge(){
         echo __METHOD__.' duoc goi';
         return $this->age;
@@ -18,9 +21,17 @@ class User{
 }
 
 class Sinhvien extends User{
-    function getName(){
-        return $this->name;
+    // public $name = "Son";
+
+    function __construct(){
+
     }
+    function getNameParent(){
+        return parent::getName();
+    }
+    // function getName(){
+    //     return $this->name;
+    // }
     function getAge(){
         echo __METHOD__.' duoc goi';
         return $this->age;
@@ -29,9 +40,14 @@ class Sinhvien extends User{
         return $this->assets;
     }
 }
+// $userA = new User('NVA');
+$svA = new Sinhvien('NVA');
+echo $svA->getNameParent();
 
-$svA = new Sinhvien;
-echo $svA->getName();
+
+
+
+// echo $svA->getName();
 
 // echo $svA->getAge();
 
