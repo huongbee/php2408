89b81a0295e8c18d834c98d6c5644baa1d2a6a99
Huong <admin@Apples-MacBook-Pro.local>
2018-10-19 19:57:37 +0700

    bind

--- PDO/index.php
+++ PDO/index.php
@@ -35,16 +35,70 @@ catch(\Exception $e){
 //         VALUES(?,?,?)";
 
 
-$data = [
-    ':image'=>'hinh-2.png',
-    ':link'=>'http://.....',
-    ':title'=>'Khuyen mai...'
-];
+// $data = [
+//     ':image'=>'hinh-2.png',
+//     ':link'=>'http://.....',
+//     ':title'=>'Khuyen mai...'
+// ];
+// $sql = "INSERT INTO slide(image,link,title) 
+// VALUES(:image,:link,:title)";
+
+// $sttm = $db->prepare($sql);
+// $check = $sttm->execute($data);
+// if($check){
+//     echo "thanh cong";
+// }
+// else{
+//     echo 'that bai';
+// }
+
+// $image = 'hinh-a.png';
+// $link = 'http://...';
+// $title = 'Hinh a';
+
+// $sql = "INSERT INTO slide(image,link,title) 
+// VALUES(?,?,?)";
+
+// $sttm = $db->prepare($sql);
+
+// $sttm->bindParam(1,$image);
+// $sttm->bindParam(2,$link);
+// $sttm->bindParam(3,$title);
+
+// // $sttm->bindValue(1,$image);
+// // $sttm->bindValue(2,$link);
+// // $sttm->bindValue(3,$title);
+
+// $check = $sttm->execute();
+
+// if($check){
+//     echo "thanh cong";
+// }
+// else{
+//     echo 'that bai';
+// }
+
+
+
+$image = 'hinh-a.png';
+$link = 'http://...';
+$title = 'Hinh a';
+
 $sql = "INSERT INTO slide(image,link,title) 
 VALUES(:image,:link,:title)";
 
 $sttm = $db->prepare($sql);
-$check = $sttm->execute($data);
+
+$sttm->bindParam(':image',$image);
+$sttm->bindParam(':link',$link);
+$sttm->bindParam(':title',$title);
+
+// $sttm->bindValue(':image','Hinh-c.gif');
+// $sttm->bindValue(':link','https://thegioididong.com/');
+// $sttm->bindValue(':title','Mung 20/10');
+
+$check = $sttm->execute();
+
 if($check){
     echo "thanh cong";
 }
